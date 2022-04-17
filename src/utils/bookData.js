cbe5d6e7ef19414fa0e403970a586156bd3fdc17
danny <bahuko@gmail.com>
2022-04-17 23:01:42 +0200

    remove catalogue id check

--- src/utils/bookData.js
+++ src/utils/bookData.js
@@ -1,35 +1,23 @@
 export const hasBookChanged = (prev, curr) => {
-  // if catalogue's ID has changed, it means that the book has been reassigned
-  // and it needs to be updated
-  if (prev.catalogue.id !== curr.catalogue.id) {
-    return true;
-  }
-
-  // divide book data keys by value type (string/array of strings)...
+  // Divide book data keys by value type (string/array of strings)...
   const stringPropKeys = [];
   const arrayPropKeys = [];
-  Object.keys(prev.entryDetails).forEach((key) =>
-    typeof prev.entryDetails[key] === "string"
+  Object.keys(prev).forEach((key) =>
+    typeof prev[key] === "string"
       ? stringPropKeys.push(key)
       : arrayPropKeys.push(key)
   );
-  // ...to check if any of the book data props have changed
-  // a) strings:
-  if (
-    stringPropKeys.some(
-      (key) => prev.entryDetails[key] !== curr.entryDetails[key]
-    )
-  ) {
+  // ...to check if any of the props have changed:
+  // a) strings
+  if (stringPropKeys.some((key) => prev[key] !== curr[key])) {
     return true;
   }
-  // b) arrays of strings:
+  // b) arrays of strings
   if (
     arrayPropKeys.some(
       (key) =>
-        curr.entryDetails[key].length !== prev.entryDetails[key].length ||
-        !curr.entryDetails[key].every((item) =>
-          prev.entryDetails[key].includes(item)
-        )
+        curr[key].length !== prev[key].length ||
+        !curr[key].every((item) => prev[key].includes(item))
     )
   ) {
     return true;
